4d4eb310e398889037645e5dd5abbc0cc2ce7248
aishwarya <ashp17star@gmail.com>
2024-05-23 17:35:35 +0530

    View Type and position specific data in recycler view

--- app/src/main/java/in/bitcode/a10_04_24_recyclerviewdemo_kotlinversion4/AdapterImplementation.kt
+++ app/src/main/java/in/bitcode/a10_04_24_recyclerviewdemo_kotlinversion4/AdapterImplementation.kt
@@ -1,5 +1,6 @@
 package `in`.bitcode.a10_04_24_recyclerviewdemo_kotlinversion4
 
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
@@ -10,6 +11,15 @@ import androidx.recyclerview.widget.RecyclerView.ViewHolder
 class AdapterImplementation(private var posts : ArrayList<Post>,
                             private var advertisements : ArrayList<Advertisement>) :
                             RecyclerView.Adapter<ViewHolder>() {
+
+                                interface OnPostClickListener{
+                                    fun onPostClick(position : Int, post : Post)
+                                }
+
+    var onPostClickListener : OnPostClickListener? = null
+
+    val VIEW_TYPE_POST = 1
+    val VIEW_TYPE_ADVT = 2
 
     inner class PostViewHolder(private val view: View) : RecyclerView.ViewHolder(view) {
         var txtUsername : TextView
@@ -31,6 +41,12 @@ class AdapterImplementation(private var posts : ArrayList<Post>,
 //        }
     }
 
+    interface OnAdvertisementClickListener{
+         fun onAdvertisementClick(position : Int, advertisement: Advertisement)
+    }
+
+    var advertisement : Advertisement? = null
+
     inner class AdvertisementViewHolder(private  val view : View) : RecyclerView.ViewHolder(view){
         var txtAdvertisement : TextView
         init {
@@ -39,16 +55,39 @@ class AdapterImplementation(private var posts : ArrayList<Post>,
     }
 
     override fun getItemCount(): Int {
-        TODO("Not yet implemented")
+       return posts.size + advertisements.size
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        TODO("Not yet implemented")
+       if (viewType == VIEW_TYPE_POST){
+           var layoutInflater = LayoutInflater.from(parent.context)
+           var postView = layoutInflater.inflate(R.layout.post_view,null)
+           return PostViewHolder(postView)
+       } else {
+           var layoutInflater = LayoutInflater.from(parent.context)
+           var advertisementView = layoutInflater.inflate(R.layout.advertisement_view, null)
+           return AdvertisementViewHolder(advertisementView)
+       }
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        TODO("Not yet implemented")
+        if (holder is PostViewHolder){
+            val post = posts[position - position/3]
+            holder.txtUsername.text = post.username
+            holder.txtPostTitle.text = post.title
+            holder.imagePost.setImageResource(post.imageId)
+        }
+        if (holder is AdvertisementViewHolder){
+            val advertisement = advertisements[position/2 - 1]
+            holder.txtAdvertisement.text = advertisement.title
+        }
     }
 
-
+    override fun getItemViewType(position: Int): Int {
+        return if(position % 3 == 2){
+            VIEW_TYPE_ADVT
+        } else {
+            VIEW_TYPE_POST
+        }
+    }
 }
--- app/src/main/java/in/bitcode/a10_04_24_recyclerviewdemo_kotlinversion4/MainActivity.kt
+++ app/src/main/java/in/bitcode/a10_04_24_recyclerviewdemo_kotlinversion4/MainActivity.kt
@@ -5,11 +5,42 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var recyclerViewPostAdvt: RecyclerView
+    private val posts = ArrayList<Post>()
+    private val advertisements = ArrayList<Advertisement>()
+    private lateinit var postsAdapter : AdapterImplementation
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+        initData()
+        initViews()
+    }
+
+    fun initViews(){
+        recyclerViewPostAdvt = findViewById(R.id.recyclerViewPostAdvt)
+        recyclerViewPostAdvt.layoutManager = LinearLayoutManager(
+            this,
+            LinearLayoutManager.VERTICAL,
+            false
+        )
+            postsAdapter = AdapterImplementation(posts,advertisements)
+            recyclerViewPostAdvt.adapter = postsAdapter
+    }
+
+    fun initData(){
+        for (i in 1..10){
+            posts.add(
+                Post("post $i", "postTitle $i",R.drawable.ic_launcher_background)
+            )
+        }
+
+        for (i in 1..10){
+            advertisements.add(Advertisement("adv $i ", "AdvUrl $i"))
+        }
     }
 }
